4c57610428a42d373c4b9f7d2d29f25432643120
iamVictorSam <iamvictorsam@gmail.com>
2022-01-13 21:45:59 +0100

    update4

--- app/js/script.js
+++ app/js/script.js
@@ -3,6 +3,16 @@ const header = document.querySelector('.header');
 const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
+const zIndex = document.querySelector('.objective__zIndex');
+const image1 = document.querySelector('.objective__image');
+const image2 = document.querySelector('.objective__image2');
+const image3 = document.querySelector('.objective__image3');
+const content1 = document.querySelector('#content1');
+const content2 = document.querySelector('#content2');
+const content3 = document.querySelector('#content3');
+const number1 = document.querySelector('#number1');
+const number2 = document.querySelector('#number2');
+const number3 = document.querySelector('#number3');
 
 btnHamburger.addEventListener('click', function () {
    console.log('this');
@@ -24,4 +34,52 @@ btnHamburger.addEventListener('click', function () {
     });
        
    }
+})
+
+content1.addEventListener('mouseover', function(e) {
+    console.log('e.')
+    image1.style.zIndex = "1";
+    content1.style.color = "black";
+    number1.style.background = "black";
+})
+
+content1.addEventListener('mouseout', function(e) {
+    console.log('e.')
+    image1.style.zIndex = "0";
+    content1.classList.add('content-out');
+    content1.style.color = "grey";
+    number1.style.background = "grey";
+
+})
+
+content2.addEventListener('mouseover', function(e) {
+    console.log('e.')
+    image2.style.zIndex = "1";
+    content2.style.color = "black";
+    number2.style.background = "black";
+
+})
+
+content2.addEventListener('mouseout', function(e) {
+    console.log('e.')
+    image2.style.zIndex = "0";
+    content2.style.color = "grey";
+    number2.style.background = "grey";
+
+})
+
+content3.addEventListener('mouseover', function(e) {
+    console.log(e.target)
+    // image1.classList.add('zIndex');
+    image3.style.zIndex = "1";
+    content3.style.color = "black";
+    number3.style.background = "black";
+
+})
+content3.addEventListener('mouseout', function(e) {
+    console.log(e.target)
+    image3.style.zIndex = "0";
+    content3.style.color = "grey";
+    number3.style.background = "grey";
+
 })
